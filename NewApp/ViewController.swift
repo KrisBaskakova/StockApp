3b2fbb72a50dace5781e3644487fd7a005d98c43
KrisBaskakova <kdbaskakova27@gmail.com>
2022-12-20 17:25:24 -0800

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by Кристина Баскакова on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
